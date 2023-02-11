13379f5da5a6be5bf6a0e4e21144065cf9348158
HanSeongLee <jkhanseong@gmail.com>
2023-02-11 19:36:53 +0900

    ADD | Main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from 'next'
+import styles from './style.module.scss';
+import Container from 'components/Container';
+import Header from 'components/Header';
+import CalculatorContainer from 'containers/CalculatorContainer';
 
 const Home: NextPage = () => {
   return (
       <>
-        <main>
+        <main className={styles.main}>
+            <Header />
+
+            <Container>
+                <CalculatorContainer className={styles.calculatorContainer} />
+            </Container>
         </main>
       </>
   );
